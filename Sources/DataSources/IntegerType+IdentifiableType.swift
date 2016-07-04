9c7cf18f12d06b48335e91248667f510de09a31c
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2016-07-04 02:55:10 +0200

    Fixes headers.

--- Sources/DataSources/IntegerType+IdentifiableType.swift
+++ Sources/DataSources/IntegerType+IdentifiableType.swift
@@ -3,7 +3,7 @@
 //  RxDataSources
 //
 //  Created by Krunoslav Zaher on 7/4/16.
-//  Copyright © 2016 kzaher. All rights reserved.
+//  Copyright © 2016 Krunoslav Zaher. All rights reserved.
 //
 
 import Foundation
@@ -54,6 +54,6 @@ extension UInt32 : IdentifiableType {
 }
 
 extension UInt64 : IdentifiableType {
-
+    
 }
 
