796600032511438389b5922744f052d69e6cc711
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2016-11-27 11:03:09 +0100

    Adds Pod source files.

---
+++ Pods/RxSwift/RxSwift/Cancelable.swift
@@ -0,0 +1,19 @@
+//
+//  Cancelable.swift
+//  RxSwift
+//
+//  Created by Krunoslav Zaher on 3/12/15.
+//  Copyright © 2015 Krunoslav Zaher. All rights reserved.
+//
+
+import Foundation
+
+/**
+Represents disposable resource with state tracking.
+*/
+public protocol Cancelable : Disposable {
+    /**
+    - returns: Was resource disposed.
+    */
+    var isDisposed: Bool { get }
+}
