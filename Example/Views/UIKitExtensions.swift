e389133533d3117921fa4b23fed9cbc7cbf3c2cf
Stefan van den Oord <soord@mac.com>
2017-07-16 09:20:27 +0200

    Fixed warnings about redundant constraints by removing the constraints that the compiler can derive itself

--- Example/Views/UIKitExtensions.swift
+++ Example/Views/UIKitExtensions.swift
@@ -25,7 +25,7 @@ extension UITableViewCell: ReusableView {
 
 extension UITableView {
     
-    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
