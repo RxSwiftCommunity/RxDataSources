4f36f0a50fe1b1f830928df8edb8233ad8e9d6aa
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-08-05 21:08:17 +0200

    Removes warning to restart xcode.

--- dependencies.sh
+++ dependencies.sh
@@ -3,5 +3,4 @@ if [[ ( ! -d "RxSwift/.git" ) && ( ! -d "Carthage" ) && ( ! -d 'Pods' ) ]]; then
     git submodule update --init --recursive --force
     cd RxSwift
     git reset origin/master --hard
-    osascript -e 'tell app "Xcode" to display dialog "We have downloaded dependencies. Please restart Xcode"'
 fi
