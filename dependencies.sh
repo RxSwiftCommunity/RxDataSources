00688d815a5d367effca2b69d96d1c3bf47e8455
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-08-05 21:06:14 +0200

    Improves dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,7 @@
 set -e
-if [[ ( ! -d "RxSwift" ) && ( ! -d "Carthage" ) && ( ! -d 'Pods' ) ]]; then
+if [[ ( ! -d "RxSwift/.git" ) && ( ! -d "Carthage" ) && ( ! -d 'Pods' ) ]]; then
     git submodule update --init --recursive --force
     cd RxSwift
     git reset origin/master --hard
+    osascript -e 'tell app "Xcode" to display dialog "We have downloaded dependencies. Please restart Xcode"'
 fi
