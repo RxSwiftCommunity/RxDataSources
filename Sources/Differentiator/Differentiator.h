c883beb687d288b55d61c90d75059ed5fcb63c1c
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2017-08-09 21:36:54 +0200

    Target cleanup.

--- Sources/Differentiator/Differentiator.h
+++ Sources/Differentiator/Differentiator.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 kzaher. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Differentiator.
 FOUNDATION_EXPORT double DifferentiatorVersionNumber;
