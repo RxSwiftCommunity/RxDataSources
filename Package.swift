155d1022fcfd147c874da5ee6eebf30e0edae3b3
Suyeol Jeon <devxoul@gmail.com>
2017-09-24 17:41:31 +0900

    Update Package.swift to V4

--- Package.swift
+++ Package.swift
@@ -1,14 +1,19 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
   name: "RxDataSources",
-  targets: [
-    Target(name: "RxDataSources", dependencies: [.Target(name: "Differentiator")]),
-    Target(name: "Differentiator"),
+  products: [
+    .library(name: "RxDataSources", targets: ["RxDataSources"]),
+    .library(name: "Differentiator", targets: ["Differentiator"]),
   ],
   dependencies: [
-    .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3),
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("rxswift4.0-swift4.0")),
+  ],
+  targets: [
+    .target(name: "RxDataSources", dependencies: ["Differentiator"]),
+    .target(name: "Differentiator"),
+    .testTarget(name: "RxDataSourcesTests", dependencies: ["RxDataSources"]),
   ]
 )
