f10b5e33793da1fb38da582f455468f869df0758
Rowan Livingstone <rowan@trademe.co.nz>
2018-09-10 10:22:21 +1200

    Change RxCollectionViewSectionedAnimatedDataSource to allow non-throttled reload updates

--- Sources/RxDataSources/RxCollectionViewSectionedAnimatedDataSource.swift
+++ Sources/RxDataSources/RxCollectionViewSectionedAnimatedDataSource.swift
@@ -24,7 +24,11 @@ open class RxCollectionViewSectionedAnimatedDataSource<S: AnimatableSectionModel
     , RxCollectionViewDataSourceType {
     public typealias Element = [S]
     public typealias DecideViewTransition = (CollectionViewSectionedDataSource<S>, UICollectionView, [Changeset<S>]) -> ViewTransition
-
+    private enum Update {
+        case reload(collectionView: UICollectionView, newSections: [S])
+        case animated(collectionView: UICollectionView, differences: [Changeset<S>])
+    }
+    
     // animation configuration
     public var animationConfiguration: AnimationConfiguration
 
@@ -47,69 +51,49 @@ open class RxCollectionViewSectionedAnimatedDataSource<S: AnimatableSectionModel
             moveItem: moveItem,
             canMoveItemAtIndexPath: canMoveItemAtIndexPath
         )
-
-        self.partialUpdateEvent
+        
+        let cancelableAnimatedUpdates = PublishRelay<(UICollectionView, [Changeset<S>])?>()
+        self.animatedUpdates.bind(to: cancelableAnimatedUpdates).disposed(by: disposeBag)
+        let throttledAnimatedUpdates = cancelableAnimatedUpdates
             // so in case it does produce a crash, it will be after the data has changed
             .observeOn(MainScheduler.asyncInstance)
             // Collection view has issues digesting fast updates, this should
             // help to alleviate the issues with them.
             .throttle(0.5, scheduler: MainScheduler.instance)
-            .subscribe(onNext: { [weak self] event in
-                self?.collectionView(event.0, throttledObservedEvent: event.1)
-            })
-            .disposed(by: disposeBag)
+            .filter { $0 != nil }
+            .map { $0! }
+        Observable.merge([
+            self.reloadUpdates.map { Update.reload(collectionView: $0.0, newSections: $0.1) },
+            throttledAnimatedUpdates.map { Update.animated(collectionView: $0.0, differences: $0.1) }
+            ]).subscribe(onNext: { [weak self] update in
+                guard let `self` = self else { return }
+                switch update {
+                case .reload(let collectionView, let newSections):
+                    self.setSections(newSections)
+                    collectionView.reloadData()
+                    cancelableAnimatedUpdates.accept(nil) // Cancel any throttled animated updates as they are no longer valid
+                case .animated(let collectionView, let differences):
+                    for difference in differences {
+                        self.setSections(difference.finalSections)
+                        collectionView.performBatchUpdates(difference, animationConfiguration: self.animationConfiguration)
+                    }
+                }
+            }).disposed(by: self.disposeBag)
     }
-
+    
     // For some inexplicable reason, when doing animated updates first time
     // it crashes. Still need to figure out that one.
     var dataSet = false
-
+    
     private let disposeBag = DisposeBag()
-
-    // This subject and throttle are here
+    
+    private let reloadUpdates = PublishRelay<(UICollectionView, [S])>()
+    
+    // This relay and throttle are here
     // because collection view has problems processing animated updates fast.
     // This should somewhat help to alleviate the problem.
-    private let partialUpdateEvent = PublishSubject<(UICollectionView, Event<Element>)>()
-
-    /**
-     This method exists because collection view updates are throttled because of internal collection view bugs.
-     Collection view behaves poorly during fast updates, so this should remedy those issues.
-    */
-    open func collectionView(_ collectionView: UICollectionView, throttledObservedEvent event: Event<Element>) {
-        Binder(self) { dataSource, newSections in
-            let oldSections = dataSource.sectionModels
-            do {
-                // if view is not in view hierarchy, performing batch updates will crash the app
-                if collectionView.window == nil {
-                    dataSource.setSections(newSections)
-                    collectionView.reloadData()
-                    return
-                }
-                let differences = try Diff.differencesForSectionedView(initialSections: oldSections, finalSections: newSections)
-
-                switch self.decideViewTransition(self, collectionView, differences) {
-                case .animated:
-                    for difference in differences {
-                        dataSource.setSections(difference.finalSections)
-
-                        collectionView.performBatchUpdates(difference, animationConfiguration: self.animationConfiguration)
-                    }
-                case .reload:
-                    self.setSections(newSections)
-                    collectionView.reloadData()
-                }
-            }
-            catch let e {
-                #if DEBUG
-                    print("Error while binding data animated: \(e)\nFallback to normal `reloadData` behavior.")
-                    rxDebugFatalError(e)
-                #endif
-                self.setSections(newSections)
-                collectionView.reloadData()
-            }
-        }.on(event)
-    }
-
+    private let animatedUpdates = PublishRelay<(UICollectionView, [Changeset<S>])>()
+    
     open func collectionView(_ collectionView: UICollectionView, observedEvent: Event<Element>) {
         Binder(self) { dataSource, newSections in
             #if DEBUG
@@ -117,12 +101,31 @@ open class RxCollectionViewSectionedAnimatedDataSource<S: AnimatableSectionModel
             #endif
             if !self.dataSet {
                 self.dataSet = true
-                dataSource.setSections(newSections)
-                collectionView.reloadData()
+                self.reloadUpdates.accept((collectionView, newSections))
             }
             else {
-                let element = (collectionView, observedEvent)
-                dataSource.partialUpdateEvent.on(.next(element))
+                do {
+                    let oldSections = dataSource.sectionModels
+                    let differences = try Diff.differencesForSectionedView(initialSections: oldSections, finalSections: newSections)
+                    switch self.decideViewTransition(self, collectionView, differences) {
+                    case .animated:
+                        // if view is not in view hierarchy, performing batch updates will crash the app
+                        if collectionView.window == nil {
+                            self.reloadUpdates.accept((collectionView, newSections))
+                            return
+                        }
+                        dataSource.animatedUpdates.accept((collectionView, differences))
+                    case .reload:
+                        self.reloadUpdates.accept((collectionView, newSections))
+                    }
+                }
+                catch let e {
+                    #if DEBUG
+                    print("Error while binding data animated: \(e)\nFallback to normal `reloadData` behavior.")
+                    rxDebugFatalError(e)
+                    #endif
+                    self.reloadUpdates.accept((collectionView, newSections))
+                }
             }
         }.on(observedEvent)
     }
