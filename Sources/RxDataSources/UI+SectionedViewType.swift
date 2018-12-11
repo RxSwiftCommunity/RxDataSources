a5d0b791394cd51b812fdaf3ed3ecc3b287c5e3e
kim jisoo <kim@Jisoo.net>
2018-12-12 04:03:35 +0900

    Minor indentation fix (#267)

--- Sources/RxDataSources/UI+SectionedViewType.swift
+++ Sources/RxDataSources/UI+SectionedViewType.swift
@@ -57,7 +57,7 @@ extension UITableView : SectionedViewType {
         self.reloadSections(indexSet(sections), with: animationStyle)
     }
 
-  public func performBatchUpdates<S>(_ changes: Changeset<S>, animationConfiguration: AnimationConfiguration) {
+    public func performBatchUpdates<S>(_ changes: Changeset<S>, animationConfiguration: AnimationConfiguration) {
         self.beginUpdates()
         _performBatchUpdates(self, changes: changes, animationConfiguration: animationConfiguration)
         self.endUpdates()
@@ -97,7 +97,7 @@ extension UICollectionView : SectionedViewType {
         self.reloadSections(indexSet(sections))
     }
     
-  public func performBatchUpdates<S>(_ changes: Changeset<S>, animationConfiguration: AnimationConfiguration) {
+    public func performBatchUpdates<S>(_ changes: Changeset<S>, animationConfiguration: AnimationConfiguration) {
         self.performBatchUpdates({ () -> Void in
             _performBatchUpdates(self, changes: changes, animationConfiguration: animationConfiguration)
         }, completion: { (completed: Bool) -> Void in
